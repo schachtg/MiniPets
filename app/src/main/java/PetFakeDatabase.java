e0d875044f05fdfad81e7bf7722ba56c6c82ab07
Ethan Marquez <marqueze@myumanitoba.ca>
2021-02-25 20:54:27 -0600

    Second Commit

--- app/src/main/java/PetFakeDatabase.java
+++ app/src/main/java/PetFakeDatabase.java
@@ -10,32 +10,29 @@ public class PetFakeDatabase {
     private int happiness;  // Represents how happy the pet is
     private String type;    // The type of pet (eg. cat or dog)
     private int outfit;     // Id representing the outfit the pet's wearing
-    private boolean[] likedFoods;   // If the pet likes the food for some food id
+    private String likedFood;   // If the pet likes the food for some food id
     private ImageView reactionImg;  // ImageView which displays the pet's reaction
     private ImageView petImg;   // ImageView which displays the pet
+    private int coins;
 
     public final int MAX_FOODS = 3; // How many different types of food there is
     public final int MAX_HAPPINESS = 100;    // The max that the pet's happiness can go
 
     public void Pet(String newName, String newType, ImageView newReactionImg, ImageView newPetImg)
     {
-        name = newName;
-        happiness = MAX_HAPPINESS/2;
-        type = newType;
-        outfit = 0;
-        likedFoods = new boolean[MAX_FOODS];
-        reactionImg = newReactionImg;
-        petImg = newPetImg;
+        this.name = newName;
+        this.happiness = MAX_HAPPINESS/2;
+        this.type = newType;
+        this.outfit = 0;
+        this.reactionImg = newReactionImg;
 
-        // Sets the image of the pet
-        if(type.equals("Cat"))
-            petImg.setImageResource(R.drawable.cat);
+        //Set new pet image to cat for testing
+        this.petImg = newPetImg;
+        petImg.setImageResource(R.drawable.cat);
+
+        //Set favourite food to salmon
+        this.likedFood="Salmon";
+
+        this.coins=10000000;
     }
-
-    public ImageView seePet()
-    {
-        return this.petImg;
-    }
-
-
 }
