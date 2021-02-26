bda8c09ea1bfd111a73f67777e15da6d48e14696
Ethan Marquez <marqueze@myumanitoba.ca>
2021-02-25 21:52:36 -0600

    Second commit, created fake pet database for a new pet, and methods to delete an item and add one

--- app/src/main/java/ShopFakeDatabase.java
+++ app/src/main/java/ShopFakeDatabase.java
@@ -1,7 +1,51 @@
 import android.widget.ImageView;
-import com.example.minipets.Pet;
 
-public class ShopFakeDatabase
-{
+import com.example.minipets.ShopItem;
 
+public class ShopFakeDatabase {
+    private ShopItem[] availableItems; //list of available shop items
+    private ShopItem[] boughtItems; //list of purchased shop items
+    private ImageView itemImg;   // ImageView which displays the item
+    private int tokens; //token value used for purchase
+
+    public final int MAX_ITEMS = 3; //maximum number of shop items
+
+    //constructor
+    public ShopFakeDatabase(int newTokens){
+        availableItems = new ShopItem[MAX_ITEMS];
+        boughtItems = new ShopItem[MAX_ITEMS];
+        tokens = newTokens;
+
+        //initializing list of available items
+        availableItems[0] = new ShopItem("Chicken", 3);
+        availableItems[1] =  new ShopItem("Fish", 4);
+        availableItems[2] = new ShopItem("Beef", 3);
+    }
+
+    //returns a string array of the current available items
+    public String[] getAvailableItems() {
+        String[] items = new String[MAX_ITEMS];
+
+        for(int i = 0; i < MAX_ITEMS; i++){
+            items[i] = availableItems[i].getName();
+        }
+        return items;
+    }
+
+    //setter for list of bought items
+    public void setBoughtItems(ShopItem[] boughtItems) {
+        this.boughtItems = boughtItems;
+    }
+
+    public void addItem(String itemName, int itemCost, int itemSpot, int available )//replace an item with a new one
+    {
+        ShopItem newItem = new ShopItem(itemName, itemCost);
+        availableItems[itemSpot]=null;
+        availableItems[itemSpot]=newItem;
+    }
+
+    public void deleteItem(int itemSpot)//delete an item from the shop
+    {
+        availableItems[itemSpot]=null;
+    }
 }
