bf480d59a1398f5fcbcbe86afcc4d1030bc4667a
Jeeerdin <kashtonjordan@gmail.com>
2021-02-26 02:44:29 -0600

    Templated the "AllTests" method
    -Added the AllTests method, just have to actually make the tests now

---
+++ app/src/test/java/com/example/minipets/tests/AllTests.java
@@ -0,0 +1,20 @@
+package com.example.minipets.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+//Here is where we will import all of the tests that we will run (the different files).
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        //add all of the above test files here
+})
+
+
+public class AllTests {
+    //add nothing here
+}
---
+++ app/src/test/java/com/example/minipets/tests/PettingTest.java
@@ -0,0 +1,13 @@
+package com.example.minipets.tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+
+public class PettingTest {
+}
