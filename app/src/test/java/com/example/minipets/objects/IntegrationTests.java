d1e74b1f082e3bb18f5dd31d56dcd122509b3bcb
Jeeerdin <kashtonjordan@gmail.com>
2021-03-19 13:44:09 -0500

    Create IntegrationTests.java

---
+++ app/src/test/java/com/example/minipets/objects/IntegrationTests.java
@@ -0,0 +1,15 @@
+package com.example.minipets.objects;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+//import test class here
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        //add integration tests here.
+})
+
+
+public class IntegrationTests {
+}
