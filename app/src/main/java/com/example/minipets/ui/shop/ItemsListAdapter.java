a9a01c0eae1ac43fe925c849f7a9fef797874702
emmanuelodukoya <odukoyae@myumanitoba.ca>
2021-03-19 00:48:55 -0500

    completed showing current tokens, cost and happiness for shop items

---
+++ app/src/main/java/com/example/minipets/ui/shop/ItemsListAdapter.java
@@ -0,0 +1,50 @@
+package com.example.minipets.ui.shop;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.example.minipets.R;
+import com.example.minipets.objects.ShopItem;
+
+public class ItemsListAdapter extends ArrayAdapter<ShopItem> {
+
+    private Context newContext;
+    int newResource;
+
+    public ItemsListAdapter(Context context, int resource, ShopItem[] items){
+        super(context, resource, items);
+        newContext = context;
+        newResource = resource;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int pos, View convertView, ViewGroup parent){
+        //populating the shop items info
+        String name = getItem(pos).getName();
+        int cost = getItem(pos).getCost();
+        int happiness = 5;
+
+        //creating a new shop item for the text view
+        ShopItem newItem = new ShopItem(name, cost);
+
+        LayoutInflater inflater = LayoutInflater.from(newContext);
+        convertView = inflater.inflate(newResource, parent, false);
+
+        TextView textViewName = (TextView) convertView.findViewById(R.id.textView1);
+        TextView textViewCost = (TextView) convertView.findViewById(R.id.textView2);
+        TextView textViewHappiness = (TextView) convertView.findViewById(R.id.textView3);
+
+        textViewName.setText(name);
+        textViewCost.setText("Cost: "+String.valueOf(cost));
+        textViewHappiness.setText("Happiness: "+String.valueOf(happiness));
+
+        return convertView;
+    }
+}
