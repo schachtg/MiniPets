2414d64c5478ae75bcd91b038b89a6c1fa24f920
AmborskyA <amborskya@gmail.com>
2021-04-10 14:16:50 -0500

    Git insists i make this push, no idea why

--- app/src/test/java/com/example/minipets/objects/AllTests.java
+++ app/src/test/java/com/example/minipets/objects/AllTests.java
@@ -1,13 +1,13 @@
 package com.example.minipets.objects;
 
+import com.example.minipets.objects.IndividualTests.TestPet;
+import com.example.minipets.objects.IndividualTests.TestShop;
+import com.example.minipets.objects.IndividualTests.TestShopItem;
+
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 //Here is where we will import all of the tests that we will run (the different files).
-
-import com.example.minipets.objects.IndividualTests.TestPet;
-import com.example.minipets.objects.IndividualTests.TestShop;
-import com.example.minipets.objects.IndividualTests.TestShopItem;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
